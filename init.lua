32da0e9659928d0470f4fecc3276d7b247c8011a
C043 <mariofragnitoph@gmail.com>
2025-01-22 09:15:09 +0100

    Refactored harpoon configs

--- init.lua
+++ init.lua
@@ -4,6 +4,7 @@ require("autocommands")
 require("lazyPackageManager")
 
 -- NOTE: Plugin configs
+require("pluginConfigs.harpoonConfig")
 require("pluginConfigs.autotagConfig")
 require("pluginConfigs.blanklineConfig")
 require("pluginConfigs.nvimTreeConfig")
