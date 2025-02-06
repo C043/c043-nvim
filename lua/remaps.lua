70b8074114f2ff8604a33fa9d2f160b1e94a4de2
C043 <mariofragnitoph@gmail.com>
2025-02-06 09:54:06 +0100

    Changed a remap

--- lua/remaps.lua
+++ lua/remaps.lua
@@ -38,7 +38,7 @@ vim.keymap.set("n", "<Esc>", "<cmd>nohlsearch<CR>")
 -- Diagnostic keymaps
 vim.keymap.set("n", "<leader>q", vim.diagnostic.setloclist, { desc = "Open diagnostic [Q]uickfix list" })
 
-vim.keymap.set("n", "<leader>home", "<cmd>Dashboard<CR>", { desc = "Open DashBoard" })
+vim.keymap.set("n", "<leader>0", "<cmd>Dashboard<CR>", { desc = "Open DashBoard" })
 
 -- Debugger keymaps
 vim.keymap.set("n", "<F5>", function()
