7f64c9b742bfa1f77e8e0c19e24a61f6c125eba2
C043 <mariofragnitoph@gmail.com>
2025-05-04 23:12:49 +0200

    Adds mac support for jdtls loading

--- lua/pluginConfigs/nvimJavaConfig.lua
+++ lua/pluginConfigs/nvimJavaConfig.lua
@@ -1,6 +1,15 @@
 local home = os.getenv("HOME")
+local os_name = vim.loop.os_uname().sysname
+local config_platform
+if os_name == "Darwin" then
+	config_platform = "config_mac"
+elseif os_name == "Linux" then
+	config_platform = "config_linux"
+else
+	error("Unsupported OS for jdtls: " .. os_name)
+end
 local workspace_dir = home .. "/.local/share/eclipse" .. vim.fn.fnamemodify(vim.fn.getcwd(), ":p:h:t")
-local config_path = home .. "/.local/share/nvim/mason/packages/jdtls/config_linux"
+local config_path = home .. "/.local/share/nvim/mason/packages/jdtls/" .. config_platform
 local launcher_jar =
 	vim.fn.glob(home .. "/.local/share/nvim/mason/packages/jdtls/plugins/org.eclipse.equinox.launcher.jar")
 
