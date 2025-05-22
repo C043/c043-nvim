bb190b8dddb096d3dda01c67a52af05a439efb1e
C043 <mariofragnitoph@gmail.com>
2025-05-22 11:20:16 +0200

    Changed checkbox rendering in renderMarkdown plugin conf

--- lua/plugins/renderMarkdown.lua
+++ lua/plugins/renderMarkdown.lua
@@ -6,6 +6,8 @@ return {
 		-- dependencies = { 'nvim-treesitter/nvim-treesitter', 'nvim-tree/nvim-web-devicons' }, -- if you prefer nvim-web-devicons
 		---@module 'render-markdown'
 		---@type render.md.UserConfig
-		opts = {},
+		opts = {
+			checkbox = { checked = { scope_highlight = "@markup.strikethrough" } },
+		},
 	},
 }
